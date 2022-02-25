171aaf8425f792698edb5014eacf12de53b104c9
Julio <juliocsarpolmann@gmail.com>
2022-02-25 00:15:19 -0300

    firs commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#input');
+let output = document.querySelector('#result');
+const button0 = document.querySelector('#convert');
+const button1 = document.querySelector('#copy');
+
+
+button0.addEventListener('click', () => {verifyInput(input.value) ? output.innerHTML = convert(input.value) : null});
+output.addEventListener('click', () => {copyToClipboard()});
+button1.addEventListener('click', () => {copyToClipboard()});
+
+function verifyInput(input) {
+    if (!isBinary(input)) {
+        return false, alert('Please enter a valid number');
+    }
+    return true;
+}
+
+function convert(input) {
+    console.log(parseInt(input, 2))
+    return parseInt(input, 2)
+}
+
+function isBinary(input) {
+    return /^[0-1]+$/.test(input);
+}
+
+function copyToClipboard() {
+    navigator.clipboard.writeText(output.innerHTML);
+}
